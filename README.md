48a6d6125b8d6839ba26edf3000c04e3a3278c2b
Tecnodemia <79447365+Tecnodemia@users.noreply.github.com>
2025-04-21 12:16:59 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://tecnodemia.github.io/pxt-servo-270/](https://tecnodemia.github.io/pxt-servo-270/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/tecnodemia/pxt-servo-270** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/tecnodemia/pxt-servo-270** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,36 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace Servo270 {
+
+    /**
+     * Pines permitidos: P0, P1 o P2
+     */
+    export enum ServoPin {
+        //% block="P0"
+        P0 = AnalogPin.P0,
+        //% block="P1"
+        P1 = AnalogPin.P1,
+        //% block="P2"
+        P2 = AnalogPin.P2
+    }
+
+    /**
+     * Establece el ángulo del servo (0° a 270°)
+     * @param pin uno de los pines P0, P1 o P2
+     * @param angle ángulo de 0 a 270 grados
+     */
+    //% block="Servo 270 en %pin|establecer ángulo a %angle grados"
+    //% angle.min=0 angle.max=270
+    //% angle.defl=90
+    //% pin.defl=ServoPin.P2
+    export function setServoAngle(pin: ServoPin, angle: number): void {
+        let pulse = pins.map(angle, 0, 270, 500, 2500);
+        pins.servoSetPulse(pin, pulse);
+    }
+}
